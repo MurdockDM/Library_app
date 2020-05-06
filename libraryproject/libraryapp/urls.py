fe139a656fc5901e5cdc08c2d347b0625c3fd5cd
Dustin Murdock <dmurdock2@gmail.com>
2020-05-06 13:08:28 -0500

    completed ch 6 adding details links to views and templates

--- libraryproject/libraryapp/urls.py
+++ libraryproject/libraryapp/urls.py
@@ -7,11 +7,13 @@ urlpatterns = [
     path('', home, name='home'),
     path('books/', book_list, name='books'),
     path('librarians/', list_librarians, name='librarians'),
-    path('libraries/', list_libraries, name='libraries'),
+    path('libraries/', library_list, name='libraries'),
     path('logout/', logout_user, name='logout'),
     path('book/form', book_form, name='book_form'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('library/form', library_form, name='library _form'),
-
+    path('books/<int:book_id>/', book_details, name='book'),
+    path('librarians/<int:librarian_id>/', librarian_details, name='librarian'),
+    path('libraries/<int:library_id>/', library_details, name='library'),
 
 ]
--- libraryproject/libraryapp/views/libraries/list.py
+++ libraryproject/libraryapp/views/libraries/list.py
@@ -1,13 +1,11 @@
 import sqlite3
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from libraryapp.models import Library
 from ..connection import Connection
 from django.contrib.auth.decorators import login_required
-from django.urls import reverse
-from django.shortcuts import redirect
 
 @login_required
-def list_libraries(request):
+def library_list(request):
     if request.method == 'GET':
         with sqlite3.connect(Connection.db_path) as conn:
             conn.row_factory = sqlite3.Row
@@ -18,7 +16,7 @@ def list_libraries(request):
                 l.id,
                 l.name,
                 l.address
-            from libraryapp_library l;
+            from libraryapp_library l
             """)
 
             all_libraries = []
@@ -29,7 +27,6 @@ def list_libraries(request):
                 lib.id = row["id"]
                 lib.name = row["name"]
                 lib.address = row["address"]
-                
 
                 all_libraries.append(lib)
 
@@ -41,21 +38,20 @@ def list_libraries(request):
 
         return render(request, template_name, context)
 
-
-
     elif request.method == 'POST':
         form_data = request.POST
 
-    with sqlite3.connect(Connection.db_path) as conn:
-        db_cursor = conn.cursor()
+        with sqlite3.connect(Connection.db_path) as conn:
+            db_cursor = conn.cursor()
 
-        db_cursor.execute("""
-        INSERT INTO libraryapp_library
-        (
-            name, address
-        )
-        VALUES (?, ?)
-        """,
-        (form_data['name'], form_data['address']))
+            db_cursor.execute("""
+            INSERT INTO libraryapp_library
+            (
+                name,
+                address
+            )
+            VALUES (?, ?)
+            """,
+            (form_data['name'], form_data['address']))
 
-    return redirect(reverse('libraryapp:libraries'))
+        return redirect(reverse('libraryapp:libraries'))    
